b1a7bb3474f62bf770657852e9978afb39de4d5c
Zhirayr <zhirayr.harutyunyan93@gmail.com>
2022-01-04 17:36:29 +0400

    add function path id

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ let pathSvg = document.querySelectorAll('[data-path]')
 let createDropdown = document.querySelector('[data-car-dropdown]')
 let defects = document.querySelectorAll('[data-defect]')
 let defectsInfo = document.querySelector('[data-defects-info]')
-let defectsIcon = document.querySelectorAll('[data-defect-icon]')
 let cursorY = -1
 let cursorX = -1
 let pathSvgId = ''
@@ -13,14 +12,10 @@ pathSvg.forEach(item => {
     if (item.hasAttribute('id')) {
         item.style.cursor = 'pointer'
         item.addEventListener('mouseover', function () {
-            if (item.getAttribute('class', 'selected')) {
-                item.style.fill = 'rgba(169, 15, 15, .6)'
-            } else {
-                item.style.fill = 'rgba(0, 0, 0, .5)'
-            }
+            item.classList.add('select')
         })
         item.addEventListener('mouseout', function () {
-            item.style.fill = ''
+            item.classList.remove('select')
         })
         item.addEventListener('click', (event) => {
             cursorX = event.pageX
@@ -47,15 +42,15 @@ pathSvg.forEach(item => {
 
 defects.forEach(item => {
     item.addEventListener('click', () => {
-        let defactsCount = defectsInfo.childElementCount + 1
+        let randomNumber = Math.floor(Math.random() * 10000)
         let itemDefectTaxt = item.textContent
         // let itemDefectIcon = item.lastElementChild
-        // itemDefectIcon.setAttribute('data-id', defactsCount)
+        // itemDefectIcon.setAttribute('data-id', randomNumber)
         // itemDefectIcon.setAttribute('x', cursorX - 10)
         // itemDefectIcon.setAttribute('y', cursorY - 5)
         // carSvg.appendChild(itemDefectIcon)
 
-        carSvg.insertAdjacentHTML('beforeend', `<svg class="defect" onmouseover="addActiveDefect(this)" onmouseout="removeActiveDefect()" width="15" height="15" viewBox="0 0 15 15" data-id="${defactsCount}" x='${cursorX - 10}' y='${cursorY - 5}'>
+        carSvg.insertAdjacentHTML('beforeend', `<svg class="defect" onmouseover="addActiveDefect(this)" onmouseout="removeActiveDefect()" width="15" height="15" viewBox="0 0 15 15" data-id="${randomNumber}" x='${cursorX - 10}' y='${cursorY - 5}'>
         <circle  cx="7.5" cy="7.5" r="7.5"></circle>
         <text fill='white' transform="translate(3.75 11)">${itemDefectTaxt.charAt(0).toLocaleLowerCase()}</text>
         </svg>`)
@@ -67,16 +62,32 @@ defects.forEach(item => {
         })
         defectsInfo.insertAdjacentHTML('beforeend',
             `
-            <tr onmouseover="addActiveDefect(this)" onmouseout="removeActiveDefect()" data-id="${defactsCount}">
+            <tr onmouseover="addActiveDefect(this)" onmouseout="removeActiveDefect()" data-id="${randomNumber}">
                 <td class="counted"></td>
                 <td>${pathSvgId}</td>
                 <td>${itemDefectTaxt}</td>
                 <td>${cursorX}</td>
                 <td>${cursorY}</td>
+                <td onclick="removeDefectTableRow(this)" data-id="${randomNumber}" data-path-id="${pathSvgId}">X</td>
             </tr>
         `)
     })
 })
+
+
+function removeDefectTableRow(currentId){
+    let def = document.querySelectorAll('[data-id]')
+    let pathId = currentId.getAttribute('data-path-id');
+    def.forEach(item => {
+        if (item.getAttribute('data-id') == currentId.getAttribute('data-id')) {
+            item.remove()
+        }
+    })
+    if(!document.querySelector('[data-path-id=' + pathId + ']')) {
+        document.getElementById(pathId).classList.remove('selected')
+    }
+}
+
 
 function addActiveDefect(currentId) {
     let def = document.querySelectorAll('[data-id]')
